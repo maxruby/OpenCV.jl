7d4b6aa5fabb1322e08cd33196d2db9f075b80b7
kvmanohar22 <kvmanohar22@gmail.com>
2017-03-08 03:16:16 +0530

    deprecated int issue in 0.6.0

--- test/jl/tests.jl
+++ test/jl/tests.jl
@@ -142,7 +142,7 @@ function test6()
    println("test 6: Conversion of images from Images.jl to OpenCV Mat")
    filename = joinpath(Pkg.dir("OpenCV"), "./test/images/mandrill.jpg")
    image = imread(filename)
-   dst = Mat(int(rows(image)), int(cols(image)), CV_8UC1)
+   dst = Mat(Int(round(rows(image))), Int(round(cols(image))), CV_8UC1)
    dst = convertToMat(image)
 end
 
